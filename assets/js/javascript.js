260f7d64be685eadcc4c0852a5724268fc7381c7
Omar <omarta@code.edu.az>
2024-02-02 23:26:13 +0400

    ...

---
+++ assets/js/javascript.js
@@ -0,0 +1,104 @@
+"use strict";
+
+
+// let n = 21;
+
+// if(n%3 ==0 || n%7 ==0){
+//     console.log('true');
+// }
+// else{
+//     console.log('false');
+// }
+
+
+
+
+// function Factorial(n) {
+//     let answer = 1;
+//     if(n<0){
+//         return;
+//     }
+//     for (let index = 1; index <= n; index++) {
+//             answer *= index;
+//     }
+//     return answer;
+// }
+
+// let n = 5;
+
+// console.log(Factorial(n));
+
+
+
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const squareEvenNumbers = (num) => {
+//   let sum = 0;
+//   for (let i = 0; i < num.length; i++) {
+//     if (num[i] % 2 === 0) {
+//     sum += Math.pow(num[i], 2);
+//     }
+//   }
+//   return sum
+// }
+ 
+// console.log(squareEvenNumbers(num));
+
+
+
+
+// let email = 'cavid@code.edu.az';
+// let password = '12345'
+
+// function LogIN(email, password) {
+//     if(email === 'cavid@code.edu.az' && password === '12345'){
+//         console.log('Logged in');
+//     }
+//     else{
+//         console.log('The email or password is incorrect');
+//     }
+    
+// }
+
+// LogIN(email, password)
+
+
+
+
+
+
+
+// let array = [1,2,3,4,5];
+
+// function SumOfDigits(array) {
+//     sum = 0;
+//     array.forEach(element => {
+//         sum += element
+//     });
+
+//     console.log(sum);
+    
+// }
+
+// SumOfDigits(array);
+
+
+
+
+
+
+// let array = [1,2,3,4,5,6,7,8];
+
+// function countOfEvenNumbers(aray) {
+//     let count = 0
+//     aray.forEach(element => {
+//         if (element%2==0) {
+//             count += 1
+//         }
+//     });
+
+//     console.log(count);
+// }
+
+// countOfEvenNumbers(array);
